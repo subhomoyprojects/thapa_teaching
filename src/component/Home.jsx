ddbeec55ad7df7b19daf941e1c711f2613479ca8
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-02-18 21:00:03 +0530

    intional commit

---
+++ src/component/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home(){
+    return(
+        <>
+            <h1>Hello boss</h1>
+            <p>Prototype</p>
+            <ol>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+            </ol>
+        </>
+    );
+}
