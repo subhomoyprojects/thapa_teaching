6424e73692bab680401e4a8efd8a3631ddfee75b
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-02-19 12:13:22 +0530

    greetings

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 
 export default function Home(){
+    const myName = "Subhomoy";
+    let grteeing = "";
+    let cssStyle = {};
+    
+        let currentTime = new Date();
+        currentTime = currentTime.getHours();
+
+        if(currentTime >= 0 && currentTime <= 11){
+            grteeing = 'Good Morning';
+            cssStyle.color = "green";
+        }
+        else if(currentTime >= 12 && currentTime <= 19){
+            grteeing = 'Good Afternoon'
+            cssStyle.color = "orange";
+        }
+        else{
+            grteeing = 'Good Night';
+            cssStyle.color = "blue";
+        }
     return(
         <>
-            <h1>Hello boss</h1>
-            <p>Prototype</p>
-            <ol>
-                <li>Hello</li>
-                <li>Hello</li>
-                <li>Hello</li>
-                <li>Hello</li>
-                <li>Hello</li>
-            </ol>
+            <h1>My name is {myName}</h1>
+            <p>Hello Sir, <span style={cssStyle}>{grteeing}</span></p>
         </>
     );
 }
