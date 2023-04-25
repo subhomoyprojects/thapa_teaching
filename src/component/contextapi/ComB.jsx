fb0c578e54e0f556a4f6a2d274237b573dde4629
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-04-25 23:21:34 +0530

    add usecontext hooks

--- src/component/contextapi/ComB.jsx
+++ src/component/contextapi/ComB.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ConstApi } from "./ComA";
+
 import ComC from "./ComC";
 
 function ComB(props) {
+  // this is a use context hooks
+  const fName = useContext(ConstApi);
   return (
     <div>
+      <h1>
+        My name is {fName.fName} {fName.lName}
+      </h1>
       <ComC />
     </div>
   );
