93fa9a7a8155b8e16f904866c5117d810fbcde6b
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-05-09 21:46:56 +0530

    add useNavigate

--- src/component/pages/Service.jsx
+++ src/component/pages/Service.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Navigation from "../navigation/Navigation";
+import { useNavigate } from "react-router-dom";
 
 function Service(props) {
+  const navigate = useNavigate();
   return (
     <>
       <Navigation />
       <h1>Service is working</h1>
+      <button
+        type="button"
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Back
+      </button>
     </>
   );
 }
